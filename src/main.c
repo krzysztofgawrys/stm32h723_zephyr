01e84c6ca47ccd539984fd712091f0aef4ad414c
Krzysztof Gawryś <kgawrys@concisesoftware.com>
2024-03-25 12:41:05 +0100

    change board name, use USB as console

--- src/main.c
+++ src/main.c
@@ -10,30 +10,24 @@
 #include <zephyr/drivers/gpio.h>
 
 #include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/drivers/uart.h>
+
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+             "Console device is not ACM CDC UART device");
+
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
 
-/* The devicetree node identifier for the "led0" alias. */
+/* The devicetree node identifier for the "led" alias. */
 #define LED_NODE DT_ALIAS(led)
-
-// Devicetree
-#define CONSOLE_DEVICE DEVICE_DT_GET(DT_CHOSEN(zephyr_console))
-#define USB_CDC_ACM_CHECK DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
 int main(void)
 {
-    // #if (IS_ENABLED(USB_CDC_ACM_CHECK) && IS_ENABLED(CONFIG_LOG))
-    //     const struct device *const console_dev = CONSOLE_DEVICE;
-    //     uint32_t dtr_line = 0;
-
-    //     while (!dtr_line)
-    //     {
-    //         uart_line_ctrl_get(console_dev, UART_LINE_CTRL_DTR, &dtr_line);
-    //         k_sleep(K_MSEC(100));
-    //     }
-    // #endif
-
     int ret;
     bool led_state = true;
 
@@ -59,7 +53,7 @@ int main(void)
         }
 
         led_state = !led_state;
-        printf("LED state: %s\n", led_state ? "ON" : "OFF");
+        LOG_INF("LED state: %s\n", led_state ? "ON" : "OFF");
         k_sleep(K_MSEC(1000));
     }
 
